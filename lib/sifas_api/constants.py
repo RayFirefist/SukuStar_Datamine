0cca6b5449bba23ae598517e01d9889a1b646657
Anonbelle <57560995+Anonbelle@users.noreply.github.com>
2020-10-31 10:42:12 +0200

    Update constants.py

--- lib/sifas_api/constants.py
+++ lib/sifas_api/constants.py
@@ -3,21 +3,19 @@ CURRENT_VERSION_WW = '7346'
 CURRENT_VERSION_NAME_WW = '1.7.5'
 
 VERSION_CONSTANTS_WW = {'keys': [
-	0x3740c814, 0x3520951a, 0x2e6d2939,
-], 
-'Il2CppSignatures': ['5f2a88bf074fd06a65e89bcbbca2212b', 'ec683fa710a06806d7e4dba9e0c8c775b831c820', '8147b4ae3d2bc8690d5af0ce1efd6ed242fac0c4890a9835988bd7d217aac82e'], 
-'JackpotSignatures': ['52f15d0e50eae5c8cd8e9dc0c86ca2c4', 'c22b425e0c6e03c2486eca77b132d8039c173a68', '310845316c814ff6c80a921e7d1315b75b6da107da33eb56e1d88a0de6e8d6ae'], 
-'ep': 1075, 'fullEp': 'https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep1075',
-'sessionKey': bytes.fromhex('647a4c387046394a4e66536b73776748'), 'dbStr': "x'B73DA9C0EE7116836995B5ACED4AA33B095ECAF77B33605833FD759E6E743F1D'", 'versionName': '1.7.5', 'version': '7346',
-'cert': b'''-----BEGIN PUBLIC KEY-----
+    0x3740c814, 0x3520951a, 0x2e6d2939,
+],
+    'Il2CppSignatures': ['5f2a88bf074fd06a65e89bcbbca2212b', 'ec683fa710a06806d7e4dba9e0c8c775b831c820', '8147b4ae3d2bc8690d5af0ce1efd6ed242fac0c4890a9835988bd7d217aac82e'],
+    'JackpotSignatures': ['52f15d0e50eae5c8cd8e9dc0c86ca2c4', 'c22b425e0c6e03c2486eca77b132d8039c173a68', '310845316c814ff6c80a921e7d1315b75b6da107da33eb56e1d88a0de6e8d6ae'],
+    'ep': 1075, 'fullEp': 'https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep1075',
+    'sessionKey': bytes.fromhex('647a4c387046394a4e66536b73776748'), 'dbStr': "x'B73DA9C0EE7116836995B5ACED4AA33B095ECAF77B33605833FD759E6E743F1D'", 'versionName': '1.7.5', 'version': '7346',
+    'cert': b'''-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC/ZUSWq8LCuF2JclEp6uuW9+yd
 dLQvb2420+F8rxIF8+W53BiF8g9m6nCETdRw7RVnzNABevMndCCTD6oQ6a2w0Qpo
 KeT26578UCWtGp74NGg2Q2fHYFMAhTytVk48qO4ViCN3snFs0AURU06niM98MIcE
 Unj9vj6kOBlOGv4JWQIDAQAB
 -----END PUBLIC KEY-----'''
 }
-
-
 
 
 CURRENT_VERSION_JP = '8424'
@@ -25,20 +23,16 @@ CURRENT_VERSION_JP = '8424'
 CURRENT_VERSION_NAME_JP = '2.1.0'
 
 VERSION_CONSTANTS_JP = {'keys': [
-	1340875056, 315831391, 899895548
-], 
-'Il2CppSignatures': ['6417fe3a7703c81b4e1ab5d3f1e7c437', 'bc45e6a148e3e20da8792c35a9a67736a8f90583', 'a4e4505571dc866259352c9493ca4a5bdf60f5f5711c60f4ca4b0c042775565f'], 
-'JackpotSignatures': ['399191f2778deac9a94a66ba978d5527', '730f3316b99f0a9e88a350bb870c2fb9c785d6d3', '23cbd21caa9230053caf34bd04d890387f53643a4bf33b6030d172ec7d49ff04'], 
-'ep': 2001, 'fullEp': 'https://jp-real-prod-v4tadlicuqeeumke.api.game25.klabgames.net/ep2010', 
-'sessionKey': bytes.fromhex('3934753074506f675a524d65754c6441'), 'dbStr': "x'B73DA9C0EE7116836995B5ACED4AA33B095ECAF77B33605833FD759E6E743F1D'", 'versionName': '2.0.1', 'version': '34',
-'cert': b'''-----BEGIN PUBLIC KEY-----
+    1340875056, 315831391, 899895548
+],
+    'Il2CppSignatures': ['6417fe3a7703c81b4e1ab5d3f1e7c437', 'bc45e6a148e3e20da8792c35a9a67736a8f90583', 'a4e4505571dc866259352c9493ca4a5bdf60f5f5711c60f4ca4b0c042775565f'],
+    'JackpotSignatures': ['399191f2778deac9a94a66ba978d5527', '730f3316b99f0a9e88a350bb870c2fb9c785d6d3', '23cbd21caa9230053caf34bd04d890387f53643a4bf33b6030d172ec7d49ff04'],
+    'ep': 2001, 'fullEp': 'https://jp-real-prod-v4tadlicuqeeumke.api.game25.klabgames.net/ep2011',
+    'sessionKey': bytes.fromhex('3934753074506f675a524d65754c6441'), 'dbStr': "x'B73DA9C0EE7116836995B5ACED4AA33B095ECAF77B33605833FD759E6E743F1D'", 'versionName': '2.0.1', 'version': '34',
+    'cert': b'''-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC/ZUSWq8LCuF2JclEp6uuW9+yd
 dLQvb2420+F8rxIF8+W53BiF8g9m6nCETdRw7RVnzNABevMndCCTD6oQ6a2w0Qpo
 KeT26578UCWtGp74NGg2Q2fHYFMAhTytVk48qO4ViCN3snFs0AURU06niM98MIcE
 Unj9vj6kOBlOGv4JWQIDAQAB
 -----END PUBLIC KEY-----'''
 }
-
-
-
-
